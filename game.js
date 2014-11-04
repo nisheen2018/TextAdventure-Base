5cdacaef14f7fcc5997a01b86aac42f8ffc87ef9
Nisheen Ahmed <Nisheen.Ahmed1@marist.edu>
2014-11-04 15:21:42 -0500

    Experimenting with the DOM.

---
+++ game.js
@@ -0,0 +1,13 @@
+var inputBox = document.querySelector('#action');
+
+console.log( inputBox.tagName );
+console.log( inputBox.parentNode.tagName );
+
+var outputBox = document.querySelector('#scene');
+
+console.log( outputBox.tagName );
+console.log( outPut.parentNode.tagName );
+
+function addHeader ('Title Of My Text Adventure')
+var newHeader = document.createElement ('p')
+var newText = document.CreateparentNode ('Title Of My Text Adventure')
