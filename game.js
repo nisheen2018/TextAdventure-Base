35383b91e99357328b6c3507f3940d5318fa800f
nisheen2018 <Nisheen.Ahmed1@marist.edu>
2014-11-12 18:34:20 -0500

    Update game.js

--- game.js
+++ game.js
@@ -3,15 +3,26 @@ player[action](object)
 function interpret (str){
 var trimmedStr = str.trim(),
 var parts = trimmedStr.split('')
+
+}
+
+function execute = []
 var action = parts.shift();
 return [action, parts.join('')];
 }
 
-function execute = 
+function report = 
 
+
+function gameStep = (str) {
+  return [execute, interpret, report]
+}
 
 var item = document.createElement ('li')
 item.textContent = player.items[i]
 list.appendchild(item)
 
+addEventListener("keyup", function(event) {
+    if (event.keyCode == Enter)
+      document.body = "";
 
