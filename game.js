de354052320df9f349c5dd3608c7dae8dc11a151
nisheen2018 <Nisheen.Ahmed1@marist.edu>
2014-11-13 17:24:51 -0500

    Update game.js

--- game.js
+++ game.js
@@ -1,15 +1,25 @@
 var player = { items:[ ]; pickUp: function (item){this.items.push(item);}};
 
-function interpret (input){
+function interpret (input) {
 var cmd = input.trim().split('');
     return cmd;
 }
 
-function execute = []
-var action = parts.shift();
-return [action, parts.join('')];
+function execute = (cmd){
+var action = cmd[0];
+var object = cmd [1];
+var method = player [action];
+    method (object);
 }
-
+var input = document.querySelector ('input');
+input.addEventListener ('keyup', function (event){
+    if ( event.keyCode === 13 )
+    alert (this.value);
+}
+var cmd = interpret (this.value);
+    execute (cmd);
+    alert ( player.items)
+    
 function report = 
 
 
