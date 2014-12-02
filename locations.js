2cb0cbbe8629dd58b24fdcbabb2bee59ba44d5e0
Nisheen Ahmed <Nisheen.Ahmed1@marist.edu>
2014-12-02 14:34:04 -0500

    Continuing with the locations of the game.

--- locations.js
+++ locations.js
@@ -16,16 +16,26 @@ var map = {
         new Location("Central Park", " You end up in the most recreational Area Of NYC",)
         new Location ("Times Square", "You see shining lights everywhere in the most toured place in NYc")
         new Location("Grand Central Terminal", " You get off a train")
+        new Location("Deli Shop", "You enter the shop to buy a map of the city to find your way back.")
+        new Location("Underground tunnel", "You find yourself roaming aroung the tunnel to find any other patheays.")
+        new Location("Bus Stop", "You sneak inside the bus to hitch a ride to get to downtown.")
     ],
     
     var connections = [
-       [0,0,0]
-       [0,0,0]
-       [0,0,0]
+       [0,0,0,0,0,0]
+       [0,0,0,0,0,0]
+       [0,0,0,0,0,0]
+       [0,0,0,0,0,0]
+       [0,0,0,0,0,0]
+       [0,0,0,0,0,0]
     ]
 connect(map, 0, 1);
 connect(map, 1, 2);
 connect(map, 2, 0);
+connect(map, 0, 3);
+connect(map, 2, 0);
+connect(map, 3, 1);
+
 function testMap() {
     var m;
     console.log("The " + map.locations[0].name + " is adjacent to:");
